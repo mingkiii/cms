1ddea339a0ec3a13b58c70b80228d23a76da792f
mingkiii <kimchunjae92@gmail.com>
2023-06-02 22:49:55 +0900

    feat : [002-SIGN-UP-CUSTOMER] 고객에 대한 SignUp Service 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/Customer.java
@@ -0,0 +1,64 @@
+package com.zerobase.cms.user.domain.model;
+
+import com.zerobase.cms.user.domain.SignUpForm;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class)
+public class Customer extends BaseEntity{
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "필수 입력")
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "필수 입력")
+    private String name;
+
+    @NotBlank(message = "필수 입력")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요. 특수문자는 반드시 포함 해주세요.")
+    private String password;
+
+    @NotBlank(message = "필수 입력")
+    @Pattern(regexp = "^010\\d{8,9}$", message = "'-' 빼고 010으로 시작하는 10~11자리 숫자로 작성 해주세요.")
+    private String phone;
+
+    private LocalDate birth;
+
+    private LocalDateTime verifyExpiredAt;
+    private String verificationCode;
+    private boolean verify;
+
+    public static Customer from(SignUpForm form) {
+        return Customer.builder()
+            .email(form.getEmail().toLowerCase(Locale.ROOT))
+            .name(form.getName())
+            .password(form.getPassword())
+            .phone(form.getPhone())
+            .birth(form.getBirth())
+            .build();
+
+    }
+}
