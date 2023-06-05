77ed867d37bb6728a7c23568efb8fa288d261bef
mingkiii <kimchunjae92@gmail.com>
2023-06-06 02:36:44 +0900

    feat : [008-init-order-api] order api 초기 설정
    refactor : db name 변경

--- order-api/src/main/java/com/zerobase/cms/order/OrderApplication.java
+++ order-api/src/main/java/com/zerobase/cms/order/OrderApplication.java
@@ -1,25 +1,21 @@
-package com.zerobase.cms.user;
+package com.zerobase.cms.order;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ServletComponentScan
-@EntityScan("com.zerobase.cms")
 @EnableFeignClients
 @EnableJpaAuditing
-@EnableJpaRepositories
-@RequiredArgsConstructor
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@ServletComponentScan
 @SpringBootApplication
-public class UserApplication {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication.run(OrderApplication.class,args);
     }
 }
---
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.zerobase.cms.order.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "일치하는 회원이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+}
