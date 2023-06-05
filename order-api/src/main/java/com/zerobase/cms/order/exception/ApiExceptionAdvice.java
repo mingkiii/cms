98d6610af569e2c6e3bc8b9b917762841e998264
mingkiii <kimchunjae92@gmail.com>
2023-06-06 07:20:45 +0900

    feat : [009-PRODUCT-ADD] Product Add, ProductItem Add
    추가로 Exception에 대한 response의 visuality 향상

---
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ApiExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.zerobase.cms.order.exception;
+
+import com.zerobase.cms.order.exception.CustomException.CustomExceptionResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionAdvice {
+
+    @ExceptionHandler({CustomException.class})
+    public ResponseEntity<CustomException.CustomExceptionResponse>
+    exceptionHandler(HttpServletRequest request, final CustomException e) {
+        return ResponseEntity
+            .status(e.getStatus())
+            .body(CustomException.CustomExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(e.getErrorCode().name())
+                .status(e.getStatus())
+                .build());
+    }
+}
